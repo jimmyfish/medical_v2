4280242ecac58c284e23e158cb83bd924fdf3c72
Jimmy Fish <killcoder212@gmail.com>
2018-01-03 03:22:13 +0700

    add hasil input function

---
+++ src/AppBundle/Controller/Hasil/HasilListController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jimmy
+ * Date: 03/01/18
+ * Time: 3:14
+ */
+
+namespace AppBundle\Controller\Hasil;
+
+
+use AppBundle\Entity\Hasil;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HasilListController extends Controller
+{
+    public function indexAction()
+    {
+        $hasil = $this->getDoctrine()->getRepository(Hasil::class)->findAll();
+
+        return $this->render('AppBundle:hasil:index.html.twig', [
+            'hasil' => $hasil,
+        ]);
+    }
+}
